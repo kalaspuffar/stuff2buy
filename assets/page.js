1151a45824961b4d992c023f36660735eb105fc0
Daniel Persson <mailto.woden@gmail.com>
2020-03-12 23:22:51 +0100

    Some initial features.

---
+++ assets/page.js
@@ -0,0 +1,71 @@
+var articlelist = document.getElementById('articlelist');
+
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+if (!window.indexedDB) {
+    console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+}
+var request = window.indexedDB.open("Stuff2Buy", 2);
+var db;
+request.onerror = function(event) {
+  console.log("Why didn't you allow my web app to use IndexedDB?!");
+};
+request.onupgradeneeded = function(event) {
+    db = event.target.result;  
+    let articles = db.createObjectStore('articles', {autoIncrement: true});
+}
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    var tx = db.transaction("articles", "readwrite");
+    var store = tx.objectStore("articles");
+
+    var request = store.getAll();
+    request.onsuccess = function(event) {
+        var temp, item, a, i;
+        temp = document.getElementsByTagName("template")[0];
+        item = temp.content.querySelector("li");
+        var articles = event.target.result;
+        for (i = 0; i < articles.length; i++) {
+          if(i != 0) {
+              var sep = document.createElement('li');
+              sep.setAttribute('role', 'separator');
+              sep.className = 'mdc-list-divider';
+              articlelist.appendChild(sep);
+          }
+          a = document.importNode(item, true);
+          a.getElementsByTagName("img")[0].src = articles[i].img;
+          a.getElementsByTagName("span")[2].textContent = articles[i].title;
+          a.getElementsByTagName("span")[3].textContent = articles[i].desc;
+          articlelist.appendChild(a);
+        }
+    }   
+};
+
+const target = document.getElementById('urlinput');
+target.addEventListener('paste', (event) => {
+    loadUrl(event);
+});
+
+function loadUrl(e) {
+    fetch('fetchpage.php?url=' + encodeURI(e.target.value))
+        .then(resp => {
+            console.log(resp);
+            return resp.json();
+        })
+        .then(json => {
+            var tx = db.transaction("articles", "readwrite");
+            var store = tx.objectStore("articles");
+        
+            json.url = e.target.value;
+            store.add(json);
+
+            e.target.value = '';            
+        });
+}
+
+document.querySelectorAll('.mdc-text-field').forEach(
+    function(ele) {
+        mdc.textField.MDCTextField.attachTo(ele);
+    }
+);
---
+++ fetchpage.php
@@ -0,0 +1,36 @@
+<?php
+
+$opts = array(
+    'http'=>array(
+        'method'=>"GET",
+        'header'=>"Accept-language: en\r\n" .
+                "user-agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36\r\n"
+    )
+);
+
+$context = stream_context_create($opts);
+
+$content = file_get_contents($_GET["url"], false, $context);
+
+$data = array();
+
+preg_match_all('/<meta.*property="og:title".*content="(.*)"/', $content, $matches);
+if(!isset($matches[1][0])) {
+    preg_match_all('/<span.*id="productTitle".*>([^<]+)/', $content, $matches);
+}
+$data['title'] = trim($matches[1][0]);
+
+$data['desc'] = '';
+preg_match_all('/<meta.*property="og:description".*content="(.*)"/', $content, $matches);
+if(isset($matches[1][0])) {
+    $data['desc'] = trim($matches[1][0]);
+}
+
+preg_match_all('/<meta.*property="og:image".*content="(.*)"/', $content, $matches);
+if(!isset($matches[1][0])) {
+    preg_match_all('/<img.*src="([^"]+)".*id="landingImage/', $content, $matches);
+}
+
+$data['img'] = trim($matches[1][0]); 
+
+echo json_encode($data);
