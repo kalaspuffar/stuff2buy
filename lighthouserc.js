3e43fffda8d58f37d798cb48f566b7fda1426da2
Daniel Persson <mailto.woden@gmail.com>
2020-05-31 12:12:47 +0200

    Add recommended assertions.

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,9 @@ module.exports = {
         url: ['http://localhost:3000/'],
         startServerCommand: 'php -S localhost:3000',
       },
+      assert: {
+        preset: 'lighthouse:recommended',
+      },      
       upload: {
         target: 'temporary-public-storage',
       },
